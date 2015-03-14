efe9dd07f038d8a046dbf3b4b13769178b072a85
Justin <justincyork@gmail.com>
2015-03-14 18:24:35 +0000

    better ancestry ancestor coverage

--- test/scrapers/ancestry-ancestor.js
+++ test/scrapers/ancestry-ancestor.js
@@ -4,22 +4,25 @@ var path = require('path'),
     genscrape = require(path.join(__dirname, '..', '..'));
     
 describe('ancestry-ancestor', function(){
-  
+
   it('works', function(done){
-    var url = 'http://trees.ancestry.com/tree/70025770/person/30322313769',
-        filePath = path.join(__dirname, '..', 'responses', 'ancestry', 'tree', '70025770-30322313769.html');
+    var url = 'http://trees.ancestry.com/tree/70025770/person/30206952907',
+        filePath = path.join(__dirname, '..', 'responses', 'ancestry', 'tree', '70025770-30206952907.html');
     helpers.mockDom(url, filePath, function(){
       genscrape().on('data', function(data){
         expect(data).to.deep.equal({ 
-          givenName: 'Sophia',
-          familyName: 'Gunia',
-          birthDate: 'Mar 1890',
-          birthPlace: 'Poland Aust',
-          fatherGivenName: 'Joseph',
-          fatherFamilyName: 'Vannoske',
-          motherGivenName: 'Ida',
-          spouseGivenName: 'Joseph',
-          spouseFamilyName: 'Frederick'
+          givenName: 'Theodore',
+          familyName: 'Yurkiewicz',
+          birthDate: '23 Jun 1892',
+          birthPlace: 'Bialykamien, Galizien, Austria',
+          deathDate: '22 Oct 1955',
+          deathPlace: 'Northampton, Hampshire, Massachusetts, United States',
+          fatherGivenName: 'Elias',
+          fatherFamilyName: 'Yurkiewicz',
+          motherGivenName: 'Xenia',
+          motherFamilyName: 'Zuk',
+          spouseGivenName: 'Helen Gertrude',
+          spouseFamilyName: 'Zierak' 
         });
         done();
       });
