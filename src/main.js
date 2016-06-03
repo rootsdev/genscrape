a47c9ecee185a961db60ae25ad7d5c08d1bbab4c
Justin <justincyork@gmail.com>
2016-06-03 19:59:35 +0000

    delete ancestry-tree

--- src/main.js
+++ src/main.js
@@ -69,7 +69,6 @@ module.exports = genscrape;
 // Include scrapers. This is primarily done so that
 // browserify can find and include them.
 // TODO: find a method that allows us to dynamically include all scrapers
-//require('./scrapers/ancestry-tree')(register);
 //require('./scrapers/ancestry-record')(register);
 //require('./scrapers/ancestry-person')(register);
 //require('./scrapers/billiongraves')(register);
