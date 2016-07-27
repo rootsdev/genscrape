added8b046a91b3577231dca27c0575295c8b978
Justin <justincyork@gmail.com>
2016-07-27 21:00:47 +0000

    genealogieonline: finish

--- src/scrapers/genealogieonline.js
+++ src/scrapers/genealogieonline.js
@@ -12,53 +12,49 @@ module.exports = function(register){
 
 function run(emitter){
   
-  var $schemaPerson = document.querySelector('[itemtype="http://schema.org/Person"]'),
-      gedx = new GedcomX(),
-      familyName = queryPropContent($schemaPerson, 'familyName');
-
-  if (familyName) {
-    
-    var primaryPerson = GedcomX.Person().addNameFromParts({
-      'http://gedcomx.org/Given': queryPropContent($schemaPerson, 'givenName'),
-      'http://gedcomx.org/Surname': familyName
-    });
+  debug('run');
+  
+  var $schemaPerson = document.querySelector('[itemtype="http://schema.org/Person"]');
+  
+  if($schemaPerson) {
+      
+    debug('schema person');
+      
+    var gedx = new GedcomX(),
+        primaryPerson = queryPerson($schemaPerson);
+        
     gedx.addPerson(primaryPerson);
     
-    var gender = queryPropContent($schemaPerson, 'gender');
-    switch(gender){
-      case 'male':
-        primaryPerson.setGender({
-          type: 'http://gedcomx.org/Male'
-        });
-        break;
-      case 'female':
-        primaryPerson.setGender({
-          type: 'http://gedcomx.org/Female'
-        });
-        break;
-    }
+    queryPropAll($schemaPerson, 'spouse').map(queryPerson).forEach(function(spouse){
+      gedx.addPerson(spouse);
+      gedx.addRelationship({
+        type: 'http://gedcomx.org/Couple',
+        person1: primaryPerson,
+        person2: spouse
+      });
+    });
     
-    primaryPerson.addFact(queryEvent($schemaPerson, 'birth', 'http://gedcomx.org/Birth'));
-    primaryPerson.addFact(queryEvent($schemaPerson, 'death', 'http://gedcomx.org/Death'));
+    // TODO: ask Bob Coret about marriage info. It disappeared?
     
-    /*
-    var spouseGivenName=$('div[itemtype="http://schema.org/Person"]:eq(0) span[itemprop="spouse"]:eq(0) meta[itemprop="givenName"]').attr("content");
-    var spouseFamilyName=$('div[itemtype="http://schema.org/Person"]:eq(0) span[itemprop="spouse"]:eq(0) meta[itemprop="familyName"]').attr("content");
-    var marriagePlace=$('div[itemtype="http://schema.org/Person"]:eq(0) span[itemprop="marriage"] span[itemprop="location"] span[itemprop="address"] meta[itemprop="addressLocality"]').attr("content");
-    var marriageDate=$('div[itemtype="http://schema.org/Person"]:eq(0) span[itemprop="marriage"] meta[itemprop="startDate"]').attr("content");
-
-    var fathid=0;
-    var mothid=1;
-    if ($('div[itemtype="http://schema.org/Person"]:eq(0) div[itemprop="parent"]:eq(0) meta[itemprop="gender"]').attr("content")=="female") {
-    	 fathid=1;
-    	 mothid=0;
-    }
-    var fatherGivenName=$('div[itemtype="http://schema.org/Person"]:eq(0) div[itemprop="parent"]:eq('+fathid+') meta[itemprop="givenName"]').attr("content");
-    var fatherFamilyName=$('div[itemtype="http://schema.org/Person"]:eq(0) div[itemprop="parent"]:eq('+fathid+') meta[itemprop="familyName"]').attr("content");
-
-    var motherGivenName=$('div[itemtype="http://schema.org/Person"]:eq(0) div[itemprop="parent"]:eq('+mothid+') meta[itemprop="givenName"]').attr("content");
-    var motherFamilyName=$('div[itemtype="http://schema.org/Person"]:eq(0) div[itemprop="parent"]:eq('+mothid+') meta[itemprop="familyName"]').attr("content");
-    */
+    queryPropAll($schemaPerson, 'parent').map(queryPerson).forEach(function(parent){
+      gedx.addPerson(parent);
+      gedx.addRelationship({
+        type: 'http://gedcomx.org/ParentChild',
+        person1: parent,
+        person2: primaryPerson
+      });
+    });
+    
+    queryPropAll($schemaPerson, 'children').map(queryPerson).forEach(function(child){
+      gedx.addPerson(child);
+      gedx.addRelationship({
+        type: 'http://gedcomx.org/ParentChild',
+        person1: primaryPerson,
+        person2: child
+      });
+    });
+    
+    // TODO: ask Bob why children don't have givenName and familyName
    
     emitter.emit('data', gedx);
   }
@@ -66,6 +62,48 @@ function run(emitter){
   else {
     emitter.emit('noData') ;     
   }  
+}
+
+/**
+ * Get the GedcomX.Person data for schema.org Person
+ * 
+ * @param {Element} $element DOM Element representing a schema.org Person
+ * @return {GedcomX.Person}
+ */
+function queryPerson($element){
+  
+  var person = GedcomX.Person();
+  
+  var givenName = queryPropContent($element, 'givenName'),
+      familyName = queryPropContent($element, 'familyName');
+  
+  if(givenName || familyName){
+    person.addNameFromParts({
+      'http://gedcomx.org/Given': givenName,
+      'http://gedcomx.org/Surname': familyName
+    });
+  } else {
+    person.addSimpleName(queryPropContent($element, 'name'));
+  }
+  
+  var gender = queryPropContent($element, 'gender');
+  switch(gender){
+    case 'male':
+      person.setGender({
+        type: 'http://gedcomx.org/Male'
+      });
+      break;
+    case 'female':
+      person.setGender({
+        type: 'http://gedcomx.org/Female'
+      });
+      break;
+  }
+  
+  person.addFact(queryEvent($element, 'birth', 'http://gedcomx.org/Birth'));
+  person.addFact(queryEvent($element, 'death', 'http://gedcomx.org/Death'));
+  
+  return person;
 }
 
 /**
@@ -77,9 +115,6 @@ function run(emitter){
  * @return {GedcomX.Fact}
  */
 function queryEvent($element, event, type){
-  // var birthPlace=$('div[itemtype="http://schema.org/Person"]:eq(0) div[itemprop="birth"] span[itemprop="location"] span[itemprop="address"] meta[itemprop="addressLocality"]').attr("content");
-  // var birthDate=$('div[itemtype="http://schema.org/Person"]:eq(0) div[itemprop="birth"] meta[itemprop="startDate"]').attr("content");
-  
   var birthPlace = queryPropContent($element, [event + 'Place', 'address', 'addressLocality']);
   var birthDate = queryPropContent($element, event + 'Date');
   
@@ -118,7 +153,7 @@ function queryPropContent($element, name){
 /**
  * Get the a schema property
  * 
- * @param {Element} element DOM Element to search inside of
+ * @param {Element} $element DOM Element to search inside of
  * @param {String|Array} name Property name to search for
  * @return {Element}
  */
@@ -137,4 +172,15 @@ function queryProp($element, name){
     
     return $element;
   }
+}
+
+/**
+ * Get all matching schema properties
+ * 
+ * @param {Element} $element DOM Element to search inside of
+ * @param {String} name Property name to search for
+ * @return {Element}
+ */
+function queryPropAll($element, name){
+  return Array.from($element.querySelectorAll('[itemprop="' + name + '"]'));
 }
--- test/src/scrapers/genealogieonline.js
+++ test/src/scrapers/genealogieonline.js
@@ -3,8 +3,25 @@ var setupTest = require('../../testHelpers').createTestRunner('genealogieonline'
 describe.only('genealogieonline', function(){
   
   it('simple', setupTest(
-    'I15210',
+    'simple',
     'https://www.genealogieonline.nl/en/voorouders-monique-en-jan-vis/I15210.php'
+  ));
+  
+  it('child', setupTest(
+    'child',
+    'https://www.genealogieonline.nl/en/voorouders-monique-en-jan-vis/I39364.php'
+  ));
+  
+  it('children', setupTest(
+    'children',
+    'https://www.genealogieonline.nl/en/voorouders-monique-en-jan-vis/I39356.php'
+  ));
+  
+  // There is no schema.org markup for grandparents. This is mostly to make sure
+  // it doesn't intefere with parsing
+  it('grandparents', setupTest(
+    'grandparents',
+    'https://www.genealogieonline.nl/en/voorouders-monique-en-jan-vis/I39388.php'
   ));
   
 });
